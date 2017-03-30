d250e7438d4211c046b0521e1dc0694168c9124a
Morgan Mayer <mayermo@oregonstate.edu>
2017-03-30 15:09:12 -0700

    Edited n-butanol files after checking with ChemKED

--- yamlcheck.py
+++ yamlcheck.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-This script will check chemked yaml files individually. 
-Morgan Mayer 3-29-2017
+This script will check yaml files individually. 
 
 """
+import os
 
 from pyked.chemked import ChemKED
 
-validate_file= ChemKED(yaml_file="n-butanol\Stranic2012_nbutanol_xO2=0.04_phi=0.5.yaml")
+validate_file= ChemKED(yaml_file=os.path.join("n-butanol", "Stranic2012_nbutanol_xO2=0.045_phi=1.0.yaml"))
+
+#validate_file= ChemKED(yaml_file="2-butanol/2-butanol_Moss_phi_1_2-b_0_0025.yaml")
+
