26b5914eceaf66a91112eaf29b52e768e691c768
Morgan Mayer <mayermo@oregonstate.edu>
2017-03-31 11:15:58 -0700

    Added Bec yaml files for sec-b, i-b, and t-b

--- yamlcheck.py
+++ yamlcheck.py
@@ -7,7 +7,7 @@ import os
 
 from pyked.chemked import ChemKED
 
-validate_file= ChemKED(yaml_file=os.path.join("n-butanol", "Stranic2012_nbutanol_xO2=0.045_phi=1.0.yaml"))
+validate_file= ChemKED(yaml_file=os.path.join("i-butanol", "Bec_i-b_0.0141_20atm.yaml"))
 
 #validate_file= ChemKED(yaml_file="2-butanol/2-butanol_Moss_phi_1_2-b_0_0025.yaml")
 
