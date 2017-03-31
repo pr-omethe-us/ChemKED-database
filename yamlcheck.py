14e37de72ef1d73924f8aed2f7e70260852feec3
Morgan Mayer <mayermo@oregonstate.edu>
2017-03-31 14:52:13 -0700

    Modified Bec yaml files

--- yamlcheck.py
+++ yamlcheck.py
@@ -7,7 +7,7 @@ import os
 
 from pyked.chemked import ChemKED
 
-validate_file= ChemKED(yaml_file=os.path.join("i-butanol", "Bec_i-b_0.0141_20atm.yaml"))
+validate_file= ChemKED(yaml_file=os.path.join("2-butanol", "2-butanol LPST Stranic xO2_0.03_phi_1.0.yaml"))
 
 #validate_file= ChemKED(yaml_file="2-butanol/2-butanol_Moss_phi_1_2-b_0_0025.yaml")
 
