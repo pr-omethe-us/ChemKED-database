e6c4ea71b8d8e51a84fdd683c5b7efe1da549f4d
Morgan Mayer <mayermo@oregonstate.edu>
2017-04-12 12:57:08 -0700

    Revised and validated Bec n-butanol files to new format

--- yamlcheck.py
+++ yamlcheck.py
@@ -7,7 +7,6 @@ import os
 
 from pyked.chemked import ChemKED
 
-validate_file= ChemKED(yaml_file=os.path.join("2-butanol", "2-butanol LPST Stranic xO2_0.03_phi_1.0.yaml"))
+validate_file= ChemKED(yaml_file=os.path.join("n-butanol", "Bec_phi_3.34_n-b_0.0654_xO2_0.118.yaml"))
 
 #validate_file= ChemKED(yaml_file="2-butanol/2-butanol_Moss_phi_1_2-b_0_0025.yaml")
-
