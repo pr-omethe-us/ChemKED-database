4719d031fbcd53d6a24a507445e456bfccd8b8d3
Morgan Mayer <mayermo@oregonstate.edu>
2017-04-17 18:16:56 -0700

    Revised and validated four t-butanol files

--- yamlcheck.py
+++ yamlcheck.py
@@ -7,6 +7,6 @@ import os
 
 from pyked.chemked import ChemKED
 
-validate_file= ChemKED(yaml_file=os.path.join("i-butanol", "isobutanol LPST Stranic xO2_0.04_phi_1.0.yaml"))
+validate_file= ChemKED(yaml_file=os.path.join("t-butanol", "t-butanol LPST Stranic xO2_0.04_phi_1.0.yaml"))
 
 #validate_file= ChemKED(yaml_file="2-butanol/2-butanol_Moss_phi_1_2-b_0_0025.yaml")
